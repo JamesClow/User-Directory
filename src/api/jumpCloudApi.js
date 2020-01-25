2352eb139474331964ca759d710d44e4131a9e40
Colby <jcclow@hollandhart.com>
2020-01-24 18:52:01 -0700

    Improved Jump Cloud api integration.

--- src/api/jumpCloudApi.js
+++ src/api/jumpCloudApi.js
@@ -4,58 +4,66 @@ import store from "../store/index";
 import { newUser } from "@/store/modules/users/user.schema.js"
 
 export function getUserList(){
-  return new Promise((resolve, rejct)=>{
+  return new Promise((resolve, reject)=>{
     axios.get("http://localhost:8005/api/systemusers").then(response=>{
       if(response && response.data){
         resolve(injestUsers(response.data.results))
       }else{
         reject("No user data");
       }
-    }).catch(response=>{
-      reject(response);
+    }).catch(e=>{
+      reject(e.response);
     })
   })
 }
 
 export function addNewUser(user){
-  return new Promise((resolve, rejct)=>{
+  return new Promise((resolve, reject)=>{
     axios.post("http://localhost:8005/api/systemusers", user).then(response=>{
       if(response && response.data){
         resolve(response.data);
       }else{
         reject("No user data");
       }
-    }).catch(response=>{
-      reject(response);
+    }).catch(e=>{
+      reject(e.response);
     })
   })
 }
 
 export function updateUser(user){
-  return new Promise((resolve, rejct)=>{
-    axios.put("http://localhost:8005/api/systemusers/"+user.id).then(response=>{
+  return new Promise((resolve, reject)=>{
+    axios.put("http://localhost:8005/api/systemusers/"+user.id, user).then(response=>{
       if(response && response.data){
         resolve(response.data);
       }else{
         reject("No user data");
       }
-    }).catch(response=>{
-      reject(response);
+    }).catch(e=>{
+      reject(e.response);
     })
   })
 }
 
 export function deleteUser(user){
-  return new Promise((resolve, rejct)=>{
-    axios.delete("http://localhost:8005/api/systemusers/"+user.id).then(response=>{
-      if(response && response.data){
-        resolve(response.data);
-      }else{
-        reject("No user data");
-      }
-    }).catch(response=>{
-      reject(response);
-    })
+  return new Promise((resolve, reject)=>{
+    store.commit('confirm', {
+      title: "Delete User?",
+      description: "This action is not reverible!",
+      start_label: "Delete",
+      start: ()=>{
+        axios.delete("http://localhost:8005/api/systemusers/"+user.id).then(response=>{
+          if(response && response.data){
+            resolve(response.data);
+          }else{
+            reject("No user data");
+          }
+        }).catch(e=>{
+          reject(e.response);
+        })
+      },
+      stop: ()=>{}
+    });
   })
 }
 
@@ -66,7 +74,7 @@ function injestUsers(users){
       ...acc,
       [user.id]: user
     }
-  })
+  }, {})
 }
 
 // merge user with default user settings / insure that the user contains all nessesary properties
